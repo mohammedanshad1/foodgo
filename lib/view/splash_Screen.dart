b3d6deff2cce2f7399534520e4b0c97917b00215
mohammedanshads <anshad@xynosoft.com>
2026-07-01 11:41:57 +0530

    done

--- lib/view/splash_Screen.dart
+++ lib/view/splash_Screen.dart
@@ -10,18 +10,6 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
-    super.initState();
-    // Navigate to home after 2 seconds
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
-      );
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
@@ -42,31 +30,6 @@ class _SplashScreenState extends State<SplashScreen> {
               fontSize: 85.sp,
               letterSpacing: 0,
               color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-// Temporary Home Screen (remove this later when you have your actual home)
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: 1.sw,
-        height: 1.sh,
-        color: Colors.white,
-        child: Center(
-          child: Text(
-            'Welcome to Foodgo!',
-            style: TextStyle(
-              fontSize: 24.sp,
-              fontWeight: FontWeight.bold,
             ),
           ),
         ),
